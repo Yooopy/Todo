ee30741bbef8497213f13ce90b6a70a1e57296a6
Yooopy <vgeraph@yahoo.com>
2024-04-14 01:17:11 +0430

    Final

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 let textV = "";
 plusBtn = document.getElementById("footer-img-plus");
-function createDiv(text) {
-  textV = prompt("Write About Your Plans :)");
-  if (textV != "" && textV != null) {
-    x = document.createElement("div");
-    x.innerHTML =
-      "<img style=' height: 20px;margin: auto 0 auto 10px;' src='assets/images/notes.png'><p style='display: block; word-wrap: break-word;width: 78%;'>" +
-      textV +
-      "</p> <div id='in-js-div-del'> <img style='height: 20px' src=assets/images/delete.png></div>";
-    x.className = "main-divs";
-    x.setAttribute("onclick", "this.remove()");
-    y = document.getElementById("main-art");
-    y.appendChild(x);
-  }
-}
 
 // function createDiv(text) {
 //   textV = prompt("Write About Your Plans :)");
@@ -33,3 +19,27 @@ function createDiv(text) {
 //     x.remove();
 //   }
 // );
+
+function closeButt(div) {
+  div.remove();
+}
+
+function createDiv(text) {
+  divv = document.getElementById("ask-div");
+  input = document.getElementById("ask-inp");
+  textV = input.value;
+  divv.style.display = "flex";
+  if (textV != "" && textV != null) {
+    x = document.createElement("div");
+    x.innerHTML =
+      "<img style=' height: 20px;margin: auto 0 auto 10px;' src='assets/images/notes.png'><p style='display: block; word-wrap: break-word;width: 78%;'>" +
+      textV +
+      "</p> <div id='in-js-div-del'> <img style='height: 20px' src=assets/images/delete.png></div>";
+    x.className = "main-divs";
+    x.setAttribute("onclick", "this.remove()");
+    y = document.getElementById("main-art");
+    y.appendChild(x);
+    input.value = "";
+    divv.style.display = "none";
+  }
+}
