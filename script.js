de57b5e4ca0c88522bc18ab03ea35cd14106a198
Yooopy <vgeraph@yahoo.com>
2024-04-13 18:22:03 +0430

    Hehe

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 function changeImageHome() {
   let img = document.getElementById("footer-img-home");
-  img.src = "assets/images/home-w.png";
+  img.src = "/assets/images/home-w.png";
 }
 
 function changeBackImageHome() {
   let img = document.getElementById("footer-img-home");
-  img.src = "assets/images/home.png";
+  img.src = "/assets/images/home.png";
 }
 
 function changeImageInfo() {
   let img = document.getElementById("footer-img-info");
-  img.src = "assets/images/info-button.png";
+  img.src = "/assets/images/info-button.png";
 }
 
 function changeBackImageInfo() {
   let img = document.getElementById("footer-img-info");
-  img.src = "assets/images/information.png";
+  img.src = "/assets/images/information.png";
 }
 
 function changeImagePlus() {
   let img = document.getElementById("footer-img-plus");
-  img.src = "assets/images/plus (1).png";
+  img.src = "/assets/images/plus (1).png";
 }
 
 function changeBackImagePlus() {
   let img = document.getElementById("footer-img-plus");
-  img.src = "assets/images/plus.png";
+  img.src = "/assets/images/plus.png";
 }
 
 alert(document.documentElement.clientWidth);
