f91b88e7bb50e4a89bb36babe5249eb89d425781
Yooopy <vgeraph@yahoo.com>
2024-04-13 19:34:38 +0430

    final footer and header

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-function changeImageHome() {
-  let img = document.getElementById("footer-img-home");
-  img.src = "assets/images/home-w.png";
-}
+// function changeImageHome() {
+//   let img = document.getElementById("footer-img-home");
+//   img.src = "assets/images/home-w.png";
+// }
 
-function changeBackImageHome() {
-  let img = document.getElementById("footer-img-home");
-  img.src = "assets/images/home.png";
-}
+// function changeBackImageHome() {
+//   let img = document.getElementById("footer-img-home");
+//   img.src = "assets/images/home.png";
+// }
 
-function changeImageInfo() {
-  let img = document.getElementById("footer-img-info");
-  img.src = "assets/images/info-button.png";
-}
+// function changeImageInfo() {
+//   let img = document.getElementById("footer-img-info");
+//   img.src = "assets/images/info-button.png";
+// }
 
-function changeBackImageInfo() {
-  let img = document.getElementById("footer-img-info");
-  img.src = "assets/images/information.png";
-}
+// function changeBackImageInfo() {
+//   let img = document.getElementById("footer-img-info");
+//   img.src = "assets/images/information.png";
+// }
 
-function changeImagePlus() {
-  let img = document.getElementById("footer-img-plus");
-  img.src = "assets/images/plus (1).png";
-}
+// function changeImagePlus() {
+//   let img = document.getElementById("footer-img-plus");
+//   img.src = "assets/images/plus (1).png";
+// }
 
-function changeBackImagePlus() {
-  let img = document.getElementById("footer-img-plus");
-  img.src = "assets/images/plus.png";
-}
-//End of Footer Funcs :)
+// function changeBackImagePlus() {
+//   let img = document.getElementById("footer-img-plus");
+//   img.src = "assets/images/plus.png";
+// }
+// //End of Footer Funcs :)
